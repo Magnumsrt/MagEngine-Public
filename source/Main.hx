6dc7f09907e7d1d4b8ecfc655667658d3ad9cf6a
Stilic <lateman-jpeg@outlook.fr>
2022-04-22 12:06:21 +0200

    srry lol

--- source/Main.hx
+++ source/Main.hx
@@ -70,8 +70,6 @@ class Main extends Sprite
 		initialState = TitleState;
 		#end
 
-		FlxGraphic.defaultPersist = true;
-
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
 
 		MagPrefs.load();
